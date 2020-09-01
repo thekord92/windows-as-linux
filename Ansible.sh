c34aad5be8a38dcf5005b5f8d24e4d58e5af9cfe
Tigran Arabadjyan <arabadj@gmail.com>
2020-09-01 19:00:06 +0400

    fix sleep link

--- Ansible.sh
+++ Ansible.sh
@@ -7,14 +7,14 @@
 
 #===Ansible=================================================================================================================
 
-# !!! Do WSL.sh first !!!
+# !!! Follow WSL.sh first !!!
 
 # Make Sleep Always TRUE
 #
 # Do this if want to configure your WSL with Ansible
 [ ! -L "/usr/bin/sleep" ] && [ ! -f "/usr/bin/sleep.bak" ] && sudo mv /usr/bin/sleep /usr/bin/sleep.bak && \
     echo "Renamed /usr/bin/sleep"
-[ ! -L "/usr/bin/sleep" ] && [sudo ln -s /bin/true /usr/bin/sleep
+[ ! -L "/usr/bin/sleep" ] && sudo ln -s /bin/true /usr/bin/sleep
 #
 
 # Install ANSIBLE
