1ca506a7ffc3d537770f84e90295249d4cf90282
Tigran Arabadjyan <arabadj@gmail.com>
2020-09-01 16:12:55 +0400

    clean wsl

--- WSL.sh
+++ WSL.sh
@@ -77,42 +77,5 @@ sudo apt upgrade
 sudo apt install wget
 #
 
-#===Ansible=================================================================================================================
-
-# Make Sleep Always TRUE
-#
-# Do this if want to configure your WSL with Ansible
-[ ! -L "/usr/bin/sleep" ] && [ ! -f "/usr/bin/sleep.bak" ] && sudo mv /usr/bin/sleep /usr/bin/sleep.bak && \
-    echo "Renamed /usr/bin/sleep"
-[ ! -L "/usr/bin/sleep" ] && [sudo ln -s /bin/true /usr/bin/sleep
-#
-
-# Install ANSIBLE
-#
-sudo apt install ansible
-#
-
-#===Vagrant=================================================================================================================
-
-# Add Vagrant Variable: vi ~/.bashrc
-#
-# export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
-#
-
-# Logoff/Login or Get NEW BASH
-#
-/bin/bash
-#
-
-# Install Vagrant
-#
-cd ~
-wget https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
-sudo apt install ./vagrant_2.2.9_x86_64.deb
-#
-
-# !!! RUN VAGRANT PROJECTS FROM WINDOWS PROFILE PATHS ONLY !!!
-# !!!   Like: /mnt/c/Users/user/Project                    !!!
-
 #===========================================================================================================================
 
