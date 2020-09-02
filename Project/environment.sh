1013e3af87a55b016e125542887e4787f6888e56
Tigran Arabadjyan <arabadj@gmail.com>
2020-09-02 17:15:52 +0400

    define nat network

--- Project/environment.sh
+++ Project/environment.sh
@@ -10,8 +10,10 @@ export WSL_REMOTE_PORT="2222"
 #---------------------------------------------------------------------------------------------------------------------------
 export VAGRANT_HOSTNAME="vgr-project" # Set your vm name
 export VAGRANT_ID="10" # ID is appended to sbnet and port
-export VAGRANT_NETWORK_NAT="192.168.71.0/24" # Set your nat network subnet
-export VAGRANT_NETWORK_PRIVATE="192.168.73.$VAGRANT_ID" # Check with your "VirtualBox Host-Only Network" adapter subnet !!!
+export VAGRANT_NETPREFIX_NAT="192.168.71"
+export VAGRANT_NETPREFIX_PRIVATE="192.168.73" # Check with your "VirtualBox Host-Only Network" adapter subnet !!!
+export VAGRANT_NETWORK_NAT="$VAGRANT_NETPREFIX_NAT.0/24"
+export VAGRANT_NETWORK_PRIVATE="$VAGRANT_NETPREFIX_PRIVATE.$VAGRANT_ID"
 export VAGRANT_SSH_PORT="22$VAGRANT_ID" # Do not edit
 export VAGRANT_SSH_PRIVATE_KEY="/mnt/c/Users/$DEPLOYER/.ssh/id_rsa_vagrant.key"
 export VAGRANT_SSH_PUBLIC_KEY="/mnt/c/Users/$DEPLOYER/.ssh/id_rsa_vagrant.pub"
