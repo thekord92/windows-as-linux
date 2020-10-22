d5b2b262355e86d9e48bf9c4c5e9f263dd7ddc37
Tigran Arabadjyan <arabadj@gmail.com>
2020-10-22 09:12:22 +0400

    add git hints

--- Git.sh
+++ Git.sh
@@ -9,14 +9,17 @@ exit 0
 
 #===Git=====================================================================================================================
 
-# Disable global Windows Git features:
+# Disable global Windows Git features (powershell):
 #
 git.exe config --global core.filemode false
 git.exe config --global core.autocrlf false
+git.exe config --global core.ignorecase true
 
-# Unset repo Git features:
+# Unset repo Git features (bash):
 #
 git config --unset core.filemode
+git config --unset core.autocrlf
+git config --unset core.ignorecase
 
 #===========================================================================================================================
 
